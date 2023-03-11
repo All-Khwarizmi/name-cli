8a125270f987e978c52c7e3651a1def2d23448fc
Jason Suarez <swarecito@gmail.com>
2023-03-11 14:33:52 +0100

    Added index.js cli file

---
+++ index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+console.log(`
+Jason Suárez
+
+Spanish teacher | Full-stack JavaScript developer | Entrepreneur \nI create applications to make my student's learning journey smoother.
+
+👀 Portfolio : https://www.jason-suarez.com/
+🐈 Github :    https://github.com/All-Khwarizmi
+😎 Linkedin :  https://www.linkedin.com/in/jason-suarez/
+
+`);
