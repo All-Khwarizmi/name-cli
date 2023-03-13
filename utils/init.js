ec6587cbba8da6031cf7fc0aac038dd8131e0d6a
Jason Suarez <swarecito@gmail.com>
2023-03-13 10:00:27 +0100

    👌 IMPROVE: Add function

--- utils/init.js
+++ utils/init.js
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
 const chalk = require("chalk");
-const pkgJSON = require("./package.json");
+const pkgJSON = require("./../package.json");
 const welcome = require("cli-welcome");
 
+
 const init = () => {
-  welcome({
-    title: pkgJSON.name,
-    tagLine: "Get to know Jason",
-    description: pkgJSON.description,
-    version: pkgJSON.version,
-    bgColor: `#6e5494`,
-    color: `#000000`,
-    bold: true,
-    clear: true,
-  });
-  const log = console.log;
-  log(`
+    welcome({
+      title: pkgJSON.name,
+      tagLine: "Get to know Jason",
+      description: pkgJSON.description,
+      version: pkgJSON.version,
+      bgColor: `#6e5494`,
+      color: `#000000`,
+      bold: true,
+      clear: true,
+    });
+    const log = console.log;
+    log(`
 ${chalk.hex(`#6e5494`).bold.inverse(" Jason Suárez ")}
 
 ${chalk.italic(
@@ -23,21 +24,16 @@ ${chalk.italic(
 )}
 
 ${chalk.hex(`#fc5e03`).bold.inverse(` Portfolio: `)} ${chalk.dim(
-    ` https://www.jason-suarez.com/`
-  )}
+      ` https://www.jason-suarez.com/`
+    )}
 ${chalk.hex(`#0a66c2`).bold.inverse(` Linkedin: `)}  ${chalk.dim(
-    ` https://www.linkedin.com/in/jason-suarez/`
-  )}
+      ` https://www.linkedin.com/in/jason-suarez/`
+    )}
 ${chalk.hex(`#6cc644`).bold.inverse(`Github: `)}    ${chalk.dim(
-    ` https://github.com/All-Khwarizmi`
-  )}
+      ` https://github.com/All-Khwarizmi`
+    )}
 
 `);
 };
 
-// module.exports = init;
-
- init();
-module.exports = () => {
-  init();
-};
+module.exports = init
